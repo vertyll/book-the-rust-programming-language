90b56f8bb2710c75524a6b3ac20086552cc6fd6e
Mikołaj Gawron <gawrmiko@gmail.com>
2024-10-09 18:46:39 +0200

    feat: add branches

---
+++ branches/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // let number = 3;
+    //
+    // if number != 0 {
+    //     println!("True");
+    // } else {
+    //     println!("False");
+    // }
+
+    // println!("Hello, world!");
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("{number}");
+
+}
