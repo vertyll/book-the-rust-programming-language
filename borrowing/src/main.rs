312f8c81038ab70095347c308297695aeb123965
Mikołaj Gawron <gawrmiko@gmail.com>
2024-10-12 10:36:57 +0200

    feat: add borrowing

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    // let s1 = String::from("Witaj");
+    //
+    // let len = calculate_length(&s1);
+    //
+    // println!("Długość '{}' wynosi {}.", s1, len);
+
+    // let mut s = String::from("Witaj");
+    //
+    // let r1 = &s;
+    // let r2 = &s;
+    // let r3 = &mut s;
+    // // change(&mut s);
+    //
+    // println!("{}, {}, i {}", r1, r2, r3);
+
+    let _reference_to_nothing = dangle();
+}
+
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
+
+// fn change(s: &mut String) {
+//     s.push_str(" , świecie");
+// }
+
+fn dangle() -> String {
+    let s = String::from("witaj");
+
+    s
 }
